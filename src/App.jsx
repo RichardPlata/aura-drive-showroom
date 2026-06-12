0cbaf32ea46b385867c7e2e84b23e3b5e872cf13
Ricardo Plata <g2ricardogplata@gmail.com>
2026-06-11 21:58:30 -0600

    Restore showroom title

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import "./App.css";
 function App() {
   return (
     <main className="app">
+      <div className="title">
+        <h1>AURA Drive</h1>
+        <p>Interactive HID Showroom</p>
+      </div>
+
       <Showroom />
     </main>
   );
